6076638a05fcfccbd0ec2ff96d673d60e643b2ff
Alejandro Uribe <jauriberamirez@gmail.com>
2025-02-23 16:56:23 -0300

    Set up Go backend with basic server structure and handlers

---
+++ backend/handlers/home.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"encoding/json"
+	"go_rest.com/rest-ws/responses"
+	"go_rest.com/rest-ws/server"
+	"net/http"
+)
+
+func HomeHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(responses.JsonResponse{
+			Message: "Hello World!",
+			Status:  true,
+		})
+	}
+}
